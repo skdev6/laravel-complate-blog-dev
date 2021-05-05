5266112cff40518e6a977eb22f4893979c79fb64
Md Sumon Khan <63170800+skdev6@users.noreply.github.com>
2021-05-05 15:50:22 +0600

    category model, migration, controller add

---
+++ app/Http/Controllers/CaregoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Caregory;
+use Illuminate\Http\Request;
+
+class CaregoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Caregory  $caregory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Caregory $caregory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Caregory  $caregory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Caregory $caregory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Caregory  $caregory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Caregory $caregory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Caregory  $caregory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Caregory $caregory)
+    {
+        //
+    }
+}
